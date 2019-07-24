f9652393a214bdb4807afddac3395e2d85bad31b
Matt Johnson <mrjz@umich.edu>
2019-07-23 19:07:01 -0700

    wrap TimeList and TimeMap to Python

---
+++ src/py-opentimelineio/opentime-bindings/opentime_timeList.cpp
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Contributors to the OpenTimelineIO project
+
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
+
+#include "opentime_bindings.h"
+#include "opentime/timeList.h"
+#include "opentime/stringPrintf.h"
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+using namespace opentime;
+
+
+static
+std::string
+_double_vector_to_string(const std::vector<double>& values)
+{
+    if (values.empty()) {
+        return "[]";
+    }
+
+    std::ostringstream stream;
+    stream << "[";
+
+    auto iter = values.cbegin();
+    stream << *iter;
+
+    while (++iter != values.cend()) {
+        stream << ", " << *iter;
+    }
+
+    stream << "]";
+
+    return stream.str();
+}
+
+std::string
+opentime_python_str(const opentime::TimeList& tl)
+{
+    return string_printf(
+        "TimeList(%s, %g)",
+        _double_vector_to_string(tl.times()).c_str(),
+        tl.rate());
+}
+
+std::string
+opentime_python_repr(const opentime::TimeList& tl)
+{
+    return string_printf(
+        "otio.opentime.TimeList(times=%s, rate=%g)",
+        _double_vector_to_string(tl.times()).c_str(),
+        tl.rate());
+}
+
+
+void opentime_timeList_bindings(py::module m)
+{
+    py::class_<TimeList>(m, "TimeList")
+        .def(py::init<std::vector<double>, double>(),
+             "times"_a = std::vector<double>(), "rate"_a = 1.0)
+        .def_property_readonly("times", &TimeList::times)
+        .def_property_readonly("rate", &TimeList::rate)
+        .def_property_readonly("size", &TimeList::size)
+        .def("__copy__", [](const TimeList& tl) {
+                return tl;
+            })
+        .def("__deepcopy__", [](const TimeList& tl) {
+                return tl;
+            })
+        .def(py::self == py::self)
+        .def(py::self != py::self)
+        .def("__str__", [](const TimeList& tl) {
+                return opentime_python_str(tl);
+            })
+        .def("__repr__", [](const TimeList& tl) {
+            return opentime_python_repr(tl);
+            })
+        ;
+}
