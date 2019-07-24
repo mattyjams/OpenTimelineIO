16151844a93356689d940add0f4a3efbc991dec4
Matt Johnson <mrjz@umich.edu>
2019-07-23 19:02:30 -0700

    add a TimeRemap TimeEffect for translating playback head "input times" into media "output times" TimeLists

---
+++ src/opentimelineio/timeRemap.cpp
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Contributors to the OpenTimelineIO project
+
+#include "opentimelineio/timeRemap.h"
+
+namespace opentimelineio { namespace OPENTIMELINEIO_VERSION_NS {
+
+
+}} // namespace opentimelineio::OPENTIMELINEIO_VERSION_NS
