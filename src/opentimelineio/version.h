eb819a0f32ce9341048c75dbd205d737e51a6287
Matt Johnson <mrjz@umich.edu>
2019-07-23 18:52:03 -0700

    bring TimeList and TimeMap from the opentime namespace into the opentimelineio namespace

--- src/opentimelineio/version.h
+++ src/opentimelineio/version.h
@@ -10,12 +10,16 @@
 #define OPENTIMELINEIO_VERSION_NS v0_19
 
 #include "opentime/rationalTime.h"
+#include "opentime/timeList.h"
+#include "opentime/timeMap.h"
 #include "opentime/timeRange.h"
 #include "opentime/timeTransform.h"
 #include "opentime/version.h"
 
 namespace opentimelineio { namespace OPENTIMELINEIO_VERSION_NS {
 using opentime::RationalTime;
+using opentime::TimeList;
+using opentime::TimeMap;
 using opentime::TimeRange;
 using opentime::TimeTransform;
 }} // namespace opentimelineio::OPENTIMELINEIO_VERSION_NS
