ab9e3ccdaaf3f1ea8161f86de24313869c0bf367
Matt Johnson <mrjz@umich.edu>
2019-07-23 19:07:01 -0700

    wrap TimeList and TimeMap to Python

--- src/py-opentimelineio/opentime-bindings/opentime_bindings.h
+++ src/py-opentimelineio/opentime-bindings/opentime_bindings.h
@@ -5,14 +5,20 @@
 #define OTIO_OPENTIME_BINDINGS_H
 
 #include "opentime/rationalTime.h"
+#include "opentime/timeList.h"
 #include <pybind11/pybind11.h>
 #include <string>
 
 void opentime_rationalTime_bindings(pybind11::module);
+void opentime_timeList_bindings(pybind11::module);
+void opentime_timeMap_bindings(pybind11::module);
 void opentime_timeRange_bindings(pybind11::module);
 void opentime_timeTransform_bindings(pybind11::module);
 
 std::string opentime_python_str(opentime::RationalTime rt);
 std::string opentime_python_repr(opentime::RationalTime rt);
+
+std::string opentime_python_str(const opentime::TimeList& tl);
+std::string opentime_python_repr(const opentime::TimeList& tl);
 
 #endif
