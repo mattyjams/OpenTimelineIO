94ab9739da2f5f9b216ac6e5e5a7f046c93a9c00
Matt Johnson <mrjz@umich.edu>
2019-07-23 18:44:48 -0700

    add a TimeList class that stores a vector of times as doubles along with a rate for those times

---
+++ src/opentime/timeList.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "opentime/version.h"
+
+#include <vector>
+
+
+namespace opentime { namespace OPENTIME_VERSION {
+
+
+class TimeList
+{
+public:
+    explicit TimeList(
+            const std::vector<double>& times = std::vector<double>(),
+            const double rate = 1.0) :
+        _times(times),
+        _rate(rate)
+    {
+    }
+
+    const std::vector<double>& times() const {
+        return _times;
+    }
+
+    double rate() const {
+        return _rate;
+    }
+
+    size_t size() const {
+        return _times.size();
+    }
+
+    TimeList(const TimeList&) = default;
+    TimeList& operator=(const TimeList&) = default;
+
+    bool operator==(const TimeList& other) const {
+        return _times == other._times && _rate == other._rate;
+    }
+
+    bool operator!=(const TimeList& other) const {
+        return !(*this == other);
+    }
+
+private:
+    std::vector<double> _times;
+    double _rate;
+};
+
+
+} } // namespace opentimelineio::OPENTIMELINEIO_VERSION
