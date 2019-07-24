4e5afa30e2513b17a574f377f77be1fd36fc7854
Matt Johnson <mrjz@umich.edu>
2019-07-23 18:58:30 -0700

    add deserialization support for TimeList and TimeMap

--- src/opentimelineio/serializableObject.h
+++ src/opentimelineio/serializableObject.h
@@ -126,6 +126,8 @@ public:
         bool read(std::string const& key, double* dest);
         bool read(std::string const& key, std::string* dest);
         bool read(std::string const& key, RationalTime* dest);
+        bool read(std::string const& key, TimeList* dest);
+        bool read(std::string const& key, TimeMap* dest);
         bool read(std::string const& key, TimeRange* dest);
         bool read(std::string const& key, class TimeTransform* dest);
         bool read(std::string const& key, Color* dest);
@@ -139,6 +141,8 @@ public:
         bool read(std::string const& key, std::optional<int>* dest);
         bool read(std::string const& key, std::optional<double>* dest);
         bool read(std::string const& key, std::optional<RationalTime>* dest);
+        bool read(std::string const& key, std::optional<TimeList>* dest);
+        bool read(std::string const& key, std::optional<TimeMap>* dest);
         bool read(std::string const& key, std::optional<TimeRange>* dest);
         bool read(std::string const& key, std::optional<TimeTransform>* dest);
         bool read(std::string const& key, std::optional<Color>* dest);
